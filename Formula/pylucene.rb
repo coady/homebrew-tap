20a295e59ab65e99f1f1fc8eb65f320c0d176afb
Aric Coady <aric.coady@gmail.com>
2018-09-04 19:13:55 -0700

    Python multiple version support.

--- Formula/pylucene.rb
+++ Formula/pylucene.rb
@@ -5,30 +5,37 @@ class Pylucene < Formula
   sha256 "bf02da82d9bc4a578b33871699aff1125020266f2299f2d6d6008edaf48591ca"
 
   option "with-shared", "build jcc as a shared library"
+  option "without-python", "Build without python3 support"
+  option "with-python@2", "Build with python2 support"
 
   depends_on "ant" => :build
   depends_on java: "1.8+"
-  depends_on "python"
+  depends_on "python" => :recommended
+  depends_on "python@2" => :optional
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", lib/"python3.7/site-packages"
-    jcc = "JCC=python3 -m jcc --arch #{MacOS.preferred_arch}"
-    if build.with? "shared"
-      jcc << " --shared"
-      opoo "shared option requires python to be built with the same compiler: #{ENV.compiler}"
-    else
-      ENV["NO_SHARED"] = "1"
-    end
+    Language::Python.each_python(build) do |python, version|
+      ENV.prepend_create_path "PYTHONPATH", lib/"python#{version}/site-packages"
+      jcc = "JCC=python#{version} -m jcc --arch #{MacOS.preferred_arch}"
+      if build.with? "shared"
+        jcc << " --shared"
+        opoo "shared option requires python to be built with the same compiler: #{ENV.compiler}"
+      else
+        ENV["NO_SHARED"] = "1"
+      end
 
-    cd "jcc" do
-      system "python3", "setup.py", "install", "--prefix=#{prefix}"
+      cd "jcc" do
+        system python, *Language::Python.setup_install_args(prefix)
+      end
+      ENV.deparallelize  # the jars must be built serially
+      system "make", "all", "install", "INSTALL_OPT=--prefix #{prefix}", jcc, "ANT=ant", "PYTHON=python#{version}", "NUM_FILES=8"
     end
-    ENV.deparallelize  # the jars must be built serially
-    system "make", "all", "install", "INSTALL_OPT=--prefix #{prefix}", jcc, "ANT=ant", "PYTHON=python3", "NUM_FILES=8"
   end
 
   test do
-    ENV.prepend_path "PYTHONPATH", HOMEBREW_PREFIX/"lib/python3.7/site-packages"
-    system "python3", "-c", "import lucene; assert lucene.initVM()"
+    Language::Python.each_python(build) do |python, version|
+      ENV.prepend_path "PYTHONPATH", HOMEBREW_PREFIX/"lib/python#{version}/site-packages"
+      system python, "-c", "import lucene; assert lucene.initVM()"
+    end
   end
 end
