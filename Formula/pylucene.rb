6d19a0a23ff346f710ea813f3081f86a39af9ae0
Aric Coady <aric.coady@gmail.com>
2020-09-09 19:35:49 -0700

    PyLucene 8.6.1.

--- Formula/pylucene.rb
+++ Formula/pylucene.rb
@@ -1,12 +1,8 @@
 class Pylucene < Formula
   desc "Python extension for accessing Java Lucene"
   homepage "https://lucene.apache.org/pylucene/index.html"
-  url "https://downloads.apache.org/lucene/pylucene/pylucene-8.3.0-src.tar.gz"
-  sha256 "2d135f4f412673ed580a00a1191dcaf5f179f4c2bb563c91359e1c9cccaa5224"
-  head do
-    url "https://dist.apache.org/repos/dist/dev/lucene/pylucene/8.6.1-rc1/pylucene-8.6.1-src.tar.gz"
-    sha256 "194f2238912973c8cf78957da8643cc2ec0e5a03c49237abfd35ecedd70107e6"
-  end
+  url "https://downloads.apache.org/lucene/pylucene/pylucene-8.6.1-src.tar.gz"
+  sha256 "194f2238912973c8cf78957da8643cc2ec0e5a03c49237abfd35ecedd70107e6"
 
   depends_on "ant" => :build
   depends_on :java => "1.8+"
