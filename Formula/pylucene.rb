cc931ed50cf8d75fc83d28373a2e7df885fa7202
Aric Coady <aric.coady@gmail.com>
2021-12-10 18:17:32 -0800

    PyLucene 8.11.0-rc2.

--- Formula/pylucene.rb
+++ Formula/pylucene.rb
@@ -3,11 +3,15 @@ class Pylucene < Formula
   homepage "https://lucene.apache.org/pylucene/index.html"
   url "https://downloads.apache.org/lucene/pylucene/pylucene-8.9.0-src.tar.gz"
   sha256 "bb13ac205bf75ff191b649e30ed480ec7723cc8a2114106eb6963c28ad5bf8fa"
+  head do
+    url "https://dist.apache.org/repos/dist/dev/lucene/pylucene/8.11.0-rc2/pylucene-8.11.0-src.tar.gz"
+    sha256 "70894afe0a1d48efa404adb2a4d6f6f951d9f3d193027e649c536bbf1b9304aa"
+  end
 
   depends_on "ant" => :build
   depends_on "openjdk"
   depends_on "python" => :recommended
-  depends_on "python@3.8" => :optional
+  depends_on "python@3.10" => :optional
 
   def install
     ENV["JCC_JDK"] = ENV.fetch("HOMEBREW_JDK", Language::Java.java_home)
@@ -22,7 +26,7 @@ class Pylucene < Formula
       "ANT=ant",
       "PYTHON=python#{version}",
       "JCC=python#{version} -m jcc",
-      "NUM_FILES=8",
+      "NUM_FILES=10",
       "INSTALL_OPT=--prefix #{prefix}"
   end
 
