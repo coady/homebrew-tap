fa22164d9dc3cf7ae79600d131a0ce4ed0aa4c32
Aric Coady <aric.coady@gmail.com>
2022-09-13 16:17:01 -0700

    PyLucene 9.1.0.

--- Formula/pylucene.rb
+++ Formula/pylucene.rb
@@ -1,13 +1,12 @@
 class Pylucene < Formula
   desc "Python extension for accessing Java Lucene"
   homepage "https://lucene.apache.org/pylucene/index.html"
-  url "https://downloads.apache.org/lucene/pylucene/pylucene-8.11.0-src.tar.gz"
-  sha256 "70894afe0a1d48efa404adb2a4d6f6f951d9f3d193027e649c536bbf1b9304aa"
+  url "https://downloads.apache.org/lucene/pylucene/pylucene-9.1.0-src.tar.gz"
+  sha256 "e8552f3577d4d03dbf417428ac24aa17cae984b2d42790f2c6db18133db0710f"
 
-  depends_on "ant" => :build
   depends_on "openjdk"
   depends_on "python" => :recommended
-  depends_on "python@3.10" => :optional
+  depends_on "python@3.9" => :optional
 
   def install
     ENV["JCC_JDK"] = ENV.fetch("HOMEBREW_JDK", Language::Java.java_home)
@@ -19,10 +18,9 @@ class Pylucene < Formula
       system "python3", *Language::Python.setup_install_args(prefix)
     end
     system "make", "all", "install",
-      "ANT=ant",
       "PYTHON=python#{version}",
       "JCC=python#{version} -m jcc",
-      "NUM_FILES=10",
+      "NUM_FILES=16",
       "INSTALL_OPT=--prefix #{prefix}"
   end
 
