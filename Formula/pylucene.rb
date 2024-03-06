bba41e646bc7777eb168b1344afef4d224e6d35e
Aric Coady <aric.coady@gmail.com>
2024-03-05 19:10:35 -0800

    PyLucene 9.10.0.

--- Formula/pylucene.rb
+++ Formula/pylucene.rb
@@ -1,12 +1,8 @@
 class Pylucene < Formula
   desc "Python extension for accessing Java Lucene"
   homepage "https://lucene.apache.org/pylucene/index.html"
-  url "https://downloads.apache.org/lucene/pylucene/pylucene-9.7.0-src.tar.gz"
-  sha256 "94193d0a5e87d32d6d21fc2a59a76a2e8c8afb5e8e6b24c9f50755ae17a81092"
-  head do
-    url "https://dist.apache.org/repos/dist/dev/lucene/pylucene/9.10.0-rc1/pylucene-9.10.0-src.tar.gz"
-    sha256 "f41807c145cf57c8cc90134faa7e990d95c8a41f53d4b7478acec79bef64ece1"
-  end
+  url "https://downloads.apache.org/lucene/pylucene/pylucene-9.10.0-src.tar.gz"
+  sha256 "f41807c145cf57c8cc90134faa7e990d95c8a41f53d4b7478acec79bef64ece1"
 
   depends_on "python"
 
@@ -18,13 +14,16 @@ class Pylucene < Formula
     packages = lib/"python#{version}/site-packages"
     ENV.prepend_create_path "PYTHONPATH", packages
     cd "jcc" do
-      system "python", *Language::Python.setup_install_args(prefix)
+      system "pip install --prefix #{prefix} build"
+      system "python -m build -nw"
+      system "pip install --prefix #{prefix} dist/JCC-*.whl"
     end
     system "make", "all", "install",
       "PYTHON=python",
       "JCC=python -m jcc",
       "NUM_FILES=16",
       "INSTALL_OPT=--prefix #{prefix} --install-dir #{packages}"
+      "MODERN_PACKAGING=true"
   end
 
   test do
