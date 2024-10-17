7201e4deaf58d93168831818623ee9c30108652e
Aric Coady <aric.coady@gmail.com>
2024-10-16 18:49:29 -0700

    PyLucene 10.0.0-rc1.

--- Formula/pylucene.rb
+++ Formula/pylucene.rb
@@ -3,6 +3,10 @@ class Pylucene < Formula
   homepage "https://lucene.apache.org/pylucene/index.html"
   url "https://downloads.apache.org/lucene/pylucene/pylucene-9.12.0-src.tar.gz"
   sha256 "5d7223e87883e64ec0cd3e62a983a4bf50dce68c997081c8989e233b90cee115"
+  head do
+    url "https://dist.apache.org/repos/dist/dev/lucene/pylucene/10.0.0-rc1/pylucene-10.0.0-src.tar.gz"
+    sha256 "100c3d61d6799ac16b7b8c1826cddf07fb1715141ebdb0d7b8119cdd96b24574"
+  end
 
   depends_on "python"
 
